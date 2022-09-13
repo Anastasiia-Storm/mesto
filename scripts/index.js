b765055a9524bf7449728f6ed87d44750b288d61
Anastasiia-Storm <A_sstorm@mail.ru>
2022-09-13 17:52:09 +0300

    Внести корректировки в js

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,49 @@
-let overlayElement = document.querySelector('.overlay')
+let popupElement = document.querySelector('.popup')
 // Нахожу и выбираю элементы по имени класса
 let openEditButton = document.querySelector('.profile__edit-button')
 // Нахожу кнопку открытия
-let closeButton = overlayElement.querySelector('.form__close-button')
+let closeButton = popupElement.querySelector('.popup__close-button')
 // Нахожу кнопку закрытия
 
-let toggleOverlay = () => {
-    overlayElement.classList.toggle('overlay_opened');
-}
-// Функция переключить overlay
-
 openEditButton.addEventListener('click', () => {
-    toggleOverlay('')
+    popupElement.classList.add('popup_opened')
 })
-
+    
 closeButton.addEventListener('click', () => {
-    toggleOverlay('')
+    popupElement.classList.remove('popup_opened')
 })
-// Отслеживайте клик по кнопке методом addEventListener
 
 
-// Находим форму в DOM
-let formElement = document.queryCommandValue('.form');// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.form__author');// Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.form__author');// Воспользуйтесь инструментом .querySelector()
+// Находим форму в DOM 
+let ElementPopup = document.queryCommandValue('.popup__container');// Воспользуйтесь методом querySelector() 
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Получите значение полей jobInput и nameInput из свойства value
+// Находим поля формы в DOM 
+let nameInput = document.querySelector('.popup__author');// Воспользуйтесь инструментом .querySelector() 
+let jobInput = document.querySelector('.popup__description');// Воспользуйтесь инструментом .querySelector() 
+let popupSaveButton = document.querySelector('.popup__save-button');
+ 
+// Обработчик «отправки» формы, хотя пока 
+// она никуда отправляться не будет 
 
-    let profileName = document.querySelector('');
-    let profileJob = document.querySelector('');
-    // Выберите элементы, куда должны быть вставлены значения полей
+function popupSubmitHandler (evt) { 
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. 
+    console.log(nameInput.value); 
+    console.log(jobInput.value); 
+    // Получите значение полей jobInput и nameInput из свойства value 
 
-    profileName.textContent = nameInput.value
-    profileJob.textContent = jobInput.value
-    // Вставьте новые значения с помощью textContent
-}
+    let profileName = document.querySelector('.profile__name'); 
+    let profileJob = document.querySelector('.profile__description'); 
+    // Выберите элементы, куда должны быть вставлены значения полей 
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
+    profileName.textContent = nameInput.value 
+    profileJob.textContent = jobInput.value 
+    // Вставьте новые значения с помощью textContent 
+} 
 
+popupSaveButton.addEventListener('click', () => {
+    ElementPopup.classList.remove('.popup_opened')
+})
+
+// Прикрепляем обработчик к форме: 
+// он будет следить за событием “submit” - «отправка» 
+ElementPopup.addEventListener('submit', popupSubmitHandler);
