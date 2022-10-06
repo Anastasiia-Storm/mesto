30cc09a1beb45f73d06531c40572eee10ec1a232
Anastasiia-Storm <A_sstorm@mail.ru>
2022-10-06 14:24:19 +0300

    готово

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const initialCards = [
     link: 'https://vandruy.by/wp-content/uploads/2018/11/amazing-scenary-of-prince-tianzi-mountain-china-1024x683.jpeg'
   },
   {
-    name: 'Водопад Виктория на реке Замбези',
+    name: 'Водопад Виктория',
     link: 'https://vandruy.by/wp-content/uploads/2018/02/victoria-falls-and-zambezi-national-park-zimbabwe-5-min-1024x683.jpg'
   },
   {
@@ -12,11 +12,11 @@ const initialCards = [
     link: 'https://vandruy.by/wp-content/uploads/2018/11/gory-kamni-potok-stremnina.jpeg'
   },
   {
-    name: 'Национальный приморский парк Хитачи, Япония',
+    name: 'Национальный парк',
     link: 'https://i.pinimg.com/originals/a4/72/54/a472549e39762db4dfb2f3467eab7b62.jpg'
   },
   {
-    name: 'Пещеры ледника Ватнайёкюдль',
+    name: 'Пещеры',
     link: 'https://35photo.ru/photos_main/240/1202736.jpg'
   },
   {
