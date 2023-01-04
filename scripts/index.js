5f333b58c6cca109e2a637a83c0b98cd85117308
Anastasiia-Storm <A_sstorm@mail.ru>
2023-01-04 15:59:59 +0300

    создать новые файлы

--- scripts/index.js
+++ scripts/index.js
@@ -46,6 +46,35 @@ const inputJob = document.querySelector('.popup__input_type_job');
 
 const buttonAddProfileSave = cardFormModalWindow.querySelector('.popup__save-button');
 
+
+const initialCards = [
+  {
+    name: 'Горы Тяньцзи',
+    link: 'https://vandruy.by/wp-content/uploads/2018/11/amazing-scenary-of-prince-tianzi-mountain-china-1024x683.jpeg'
+  },
+  {
+    name: 'Водопад Виктория',
+    link: 'https://vandruy.by/wp-content/uploads/2018/02/victoria-falls-and-zambezi-national-park-zimbabwe-5-min-1024x683.jpg'
+  },
+  {
+    name: 'Долина Йосемити',
+    link: 'https://vandruy.by/wp-content/uploads/2018/11/gory-kamni-potok-stremnina.jpeg'
+  },
+  {
+    name: 'Национальный парк',
+    link: 'https://i.pinimg.com/originals/a4/72/54/a472549e39762db4dfb2f3467eab7b62.jpg'
+  },
+  {
+    name: 'Ледяная пещера',
+    link: 'https://35photo.ru/photos_main/240/1202736.jpg'
+  },
+  {
+    name: 'Памуккале, Турция',
+    link: 'http://www.orangesmile.com/extreme/img/main/pamukkale-travertine_1.jpg'
+  }
+];
+
+
 const validationConfig = {
   formSelector: '.popup__form', // Это объекты с селекторами формы
   inputSelector: '.popup__input',
