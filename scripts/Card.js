7bfac0ad61af91d6c32f807a65fd084143f18ff0
Anastasiia-Storm <A_sstorm@mail.ru>
2023-01-04 21:44:53 +0300

    refactor: изменить файл README.md

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,3 +1,31 @@
+const initialCards = [
+  {
+    name: 'Горы Тяньцзи',
+    link: 'https://vandruy.by/wp-content/uploads/2018/11/amazing-scenary-of-prince-tianzi-mountain-china-1024x683.jpeg'
+  },
+  {
+    name: 'Водопад Виктория',
+    link: 'https://vandruy.by/wp-content/uploads/2018/02/victoria-falls-and-zambezi-national-park-zimbabwe-5-min-1024x683.jpg'
+  },
+  {
+    name: 'Долина Йосемити',
+    link: 'https://vandruy.by/wp-content/uploads/2018/11/gory-kamni-potok-stremnina.jpeg'
+  },
+  {
+    name: 'Национальный парк',
+    link: 'https://i.pinimg.com/originals/a4/72/54/a472549e39762db4dfb2f3467eab7b62.jpg'
+  },
+  {
+    name: 'Ледяная пещера',
+    link: 'https://35photo.ru/photos_main/240/1202736.jpg'
+  },
+  {
+    name: 'Памуккале, Турция',
+    link: 'http://www.orangesmile.com/extreme/img/main/pamukkale-travertine_1.jpg'
+  }
+];
+
+
 class Card {
   constructor(name, link) { // Передаю данные в виде объекта, а в самом классе присвоить полям нужные свойства:
   this._name = name;
@@ -10,37 +38,40 @@ class Card {
   // Получаю нужную разметку
   _getTemplateCard() {
   const cardElement = document
-  .querySelector('.card__template')
-  .content
-  .querySelector('.element')
-  cloneNode(true); // Клонирую элемент
+    .querySelector('.card__template')
+    .content
+    .querySelector('.element')
+    .cloneNode(true); // Клонирую элемент
 
   return cardElement // Возвращаю разметку карточки
   }
+  
 
   // Подготовка карточки к публикации
   generateCard() {
     // Запишем разметку в приватное поле _element. 
     // Так у других элементов появится доступ к ней.
-    this._newCard = this._getTemplateCard();
+    this._element = this._getTemplateCard();
 
     // Добавим данные
-    this._newCard.querySelector('.element__photo').src = this._link;
-    this._newCard.querySelector('.element__info').textContent = this._name;
+    this._element.querySelector('.element__photo').src = this._link;
+    this._element.querySelector('.element__info').textContent = this._name;
 
     // Вернём элемент наружу
-    return this._newCard;
+    return this._element;
   }
 }
 
-initialCards.forEach((item) => {
-  // Создадим экземпляр карточки
-  const card = new Card(item.name, item.link);
-  // Создаём карточку и возвращаем наружу
-  const cardElement = card.generateCard();
 
-    // Добавляем в DOM
-    document.querySelector('.elements').append(cardElement);
-}); 
+  initialCards.forEach((item) => {
+    // Создадим экземпляр карточки
+    const card = new Card(item.name, item.link);
+    // Создаём карточку и возвращаем наружу
+    const cardElement = card.generateCard();
+  
+      // Добавляем в DOM
+      document.querySelector('.elements').append(cardElement);
+  });
+
 
 export default Card;
--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,33 @@
-import Cards from './Card.js';
+import Card from "./Card.js";
+import FormValidator from "./FormValidator.js";
 
-const initialCards = [
-  {
-    name: 'Горы Тяньцзи',
-    link: 'https://vandruy.by/wp-content/uploads/2018/11/amazing-scenary-of-prince-tianzi-mountain-china-1024x683.jpeg'
-  },
-  {
-    name: 'Водопад Виктория',
-    link: 'https://vandruy.by/wp-content/uploads/2018/02/victoria-falls-and-zambezi-national-park-zimbabwe-5-min-1024x683.jpg'
-  },
-  {
-    name: 'Долина Йосемити',
-    link: 'https://vandruy.by/wp-content/uploads/2018/11/gory-kamni-potok-stremnina.jpeg'
-  },
-  {
-    name: 'Национальный парк',
-    link: 'https://i.pinimg.com/originals/a4/72/54/a472549e39762db4dfb2f3467eab7b62.jpg'
-  },
-  {
-    name: 'Ледяная пещера',
-    link: 'https://35photo.ru/photos_main/240/1202736.jpg'
-  },
-  {
-    name: 'Памуккале, Турция',
-    link: 'http://www.orangesmile.com/extreme/img/main/pamukkale-travertine_1.jpg'
-  }
-];
+
+// const initialCards = [
+//   {
+//     name: 'Горы Тяньцзи',
+//     link: 'https://vandruy.by/wp-content/uploads/2018/11/amazing-scenary-of-prince-tianzi-mountain-china-1024x683.jpeg'
+//   },
+//   {
+//     name: 'Водопад Виктория',
+//     link: 'https://vandruy.by/wp-content/uploads/2018/02/victoria-falls-and-zambezi-national-park-zimbabwe-5-min-1024x683.jpg'
+//   },
+//   {
+//     name: 'Долина Йосемити',
+//     link: 'https://vandruy.by/wp-content/uploads/2018/11/gory-kamni-potok-stremnina.jpeg'
+//   },
+//   {
+//     name: 'Национальный парк',
+//     link: 'https://i.pinimg.com/originals/a4/72/54/a472549e39762db4dfb2f3467eab7b62.jpg'
+//   },
+//   {
+//     name: 'Ледяная пещера',
+//     link: 'https://35photo.ru/photos_main/240/1202736.jpg'
+//   },
+//   {
+//     name: 'Памуккале, Турция',
+//     link: 'http://www.orangesmile.com/extreme/img/main/pamukkale-travertine_1.jpg'
+//   }
+// ];
 
 
 /** Темплейты */
@@ -158,9 +160,10 @@ function handlePreview(data) {
     Принимает объект (name, link)
     Добавляет её в DOM */
 function render(data) {
- const cardNew = createCard(data); // Это будет новая карточка
+//  const cardNew = createCard(data); // Это будет новая карточка
+  const card = new Card(data);
 
- cardsContainer.prepend(cardNew);
+  cardsContainer.prepend(card.generateCard());
 }
 
 
@@ -224,9 +227,9 @@ function setEventListeners(card, data, cardImage) {
 }
 
 
-// initialCards.forEach(render); // forEach перебирает массив и выполняет для каждого элемента свой код.
-// // Метод forEach нужен, когда мы хотим просто пройтись по массиву и для каждого элемента что-то сделать.
-// // В данном случае с помощью forEach начинаем применять функцию createCard
+initialCards.forEach(render); // forEach перебирает массив и выполняет для каждого элемента свой код.
+// Метод forEach нужен, когда мы хотим просто пройтись по массиву и для каждого элемента что-то сделать.
+// В данном случае с помощью forEach начинаем применять функцию createCard
 
 
 buttonOpenEdit.addEventListener('click', function() {  // Добавить слушатель события 
